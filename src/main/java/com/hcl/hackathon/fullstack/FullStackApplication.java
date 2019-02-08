f4ee10c3facdc78797733199b387c4aeee426835
kirankumar <kirankumar@OM.attlocal.net>
2019-02-08 13:35:56 -0500

    Sample SpringBoot

--- src/main/java/com/hcl/hackathon/fullstack/FullStackApplication.java
+++ src/main/java/com/hcl/hackathon/fullstack/FullStackApplication.java
@@ -1,12 +1,47 @@
 package com.hcl.hackathon.fullstack;
 
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import com.hcl.hackathon.fullstack.model.Account;
+import com.hcl.hackathon.fullstack.repository.AccountRepository;
 
 @SpringBootApplication
-public class FullStackApplication {
+public class FullStackApplication extends SpringBootServletInitializer {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(FullStackApplication.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FullStackApplication.class);
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(FullStackApplication.class, args);
+		LOGGER.info("App starting..");
+		FullStackApplication app = new FullStackApplication();
+		app.configure(new SpringApplicationBuilder(SpringApplicationBuilder.class)).run(args);
 	}
+
+	@Bean
+	CommandLineRunner init(final AccountRepository accountRepository) {
+
+		return new CommandLineRunner() {
+
+			@Override
+			public void run(String... arg0) throws Exception {
+				accountRepository.save(new Account("testUser1", "password"));
+				accountRepository.save(new Account("testUser2", "password"));
+				accountRepository.save(new Account("testUser3", "password"));
+
+			}
+
+		};
+
+	}
+
 }
--- src/test/java/com/hcl/hackathon/fullstack/FullStackApplicationTests.java
+++ src/test/java/com/hcl/hackathon/fullstack/FullStackApplicationTests.java
@@ -1,13 +1,50 @@
 package com.hcl.hackathon.fullstack;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hcl.hackathon.fullstack.model.Pet;
+import com.hcl.hackathon.fullstack.services.PetService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 public class FullStackApplicationTests {
+	
+	
+	@Autowired
+    PetService petService;
+    
+    
+   @Test
+   public void  findAllPets() {
+  	List<Pet> findAllPets = petService.findAllPets();
+  	assertFalse(findAllPets.isEmpty()); 
+   }
+    
+    @Test
+    public void  findByType() {
+    	List<Pet> findAllPets = petService.findByType("DOG");
+    	assertFalse(findAllPets.isEmpty()); 
+    	 findAllPets = petService.findByType("CAT");
+    	 assertTrue(findAllPets.isEmpty());
+    }
+    
+    @Test
+    public void  findByBreed() {
+  	List<Pet> findAllPets = petService.findByBreed("DOG", "Labrador");
+   	assertFalse(findAllPets.isEmpty()); 
+   }
 
 	@Test
 	public void contextLoads() {
